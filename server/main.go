163f89e911a9c4db02e31d6a8838074717297c4a
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-06 12:26:11 +0300

    add simple golang http serve for client

---
+++ server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("../client/build", true)))
+
+	// Start and run the server
+	router.Run(":5000")
+}
