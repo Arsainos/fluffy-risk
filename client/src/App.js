4d9118a1909650117274269e36c47fbaf5b14fef
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-11 22:11:54 +0300

    <client> add lazy-suspense loading of containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 // import components
 import Layout from './components/Layout/Layout';
+import Spinner from './components/UI/Spinner/spinner';
+
+// lazy loading and code splitting for containers
+const Clients = lazy(() => import('./containers/Clients/clients'));
+const Ranking = lazy(() => import('./containers/Ranking/ranking'));
+const Srp = lazy(() => import('./containers/SRP/srp'));
+const Users = lazy(() => import('./containers/Users/users'));
 
 function App() {
   return (
     <Layout>
-        <p>Hello world</p>
+        <Suspense fallback={Spinner}>
+          <React.Fragment>
+            <Ranking />
+            <Srp />
+            <Clients />
+            <Users />
+          </React.Fragment>
+        </Suspense>
     </Layout>
   );
 }
