446bd4b1445d472fb6fb3d9952ff80518e58a971
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-15 23:11:14 +0300

    <client> mock data for clients data

---
+++ client/src/mocks/data/clients.js
@@ -0,0 +1,4 @@
+export const clientsMockData = [
+    {clientName: 'Рога и Копыта',clientInn: '123456', clientHolding: 'Кококо и Петушки'},
+    {clientName: 'Напитки из Черноголовки',clientInn: '123456', clientHolding: 'Лимонад и сладкая вода'}
+]
