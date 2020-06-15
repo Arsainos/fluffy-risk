850d463b5786d28064322e975da527db05e377b8
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-15 23:10:38 +0300

    <client> clientInn fix

--- client/src/containers/Clients/Client/client.jsx
+++ client/src/containers/Clients/Client/client.jsx
@@ -35,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Client({type}) {
   const [TextFieldsInputs, setTextFieldInputs] = useState({
     clientName: '',
-    ldapUser: '',
+    clientInn: '',
     clientHolding: '',
   });
 
