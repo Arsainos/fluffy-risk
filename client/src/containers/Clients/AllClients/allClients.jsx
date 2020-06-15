77c8ad29a751b8c39caa95e9f4426e6ef363f803
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-15 23:11:33 +0300

    <client> Simple AllClient Page Component

--- client/src/containers/Clients/AllClients/allClients.jsx
+++ client/src/containers/Clients/AllClients/allClients.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+// import components
+import MuiTable from '../../../components/UI/Table/table';
+
+// import data for table
+import {clientsMockData} from '../../../mocks/data/clients';
+
 export default function AllClients() {
     return (
-        <div>
-            <h1>Все пользователи</h1>
-        </div>
+        <MuiTable 
+            columns={[
+                { title: 'Имя клиента', field: 'clientName'},
+                { title: 'ИНН', field: 'clientInn'},
+                { title: 'Группа', field: 'clientHolding'}
+            ]}
+            data={clientsMockData}
+            title={'Все клиенты'}
+        />
     )
 }
