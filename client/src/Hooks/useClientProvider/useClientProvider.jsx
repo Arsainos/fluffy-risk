6b3d9eb836861b335b6186724be2f44cc6129089
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-17 16:33:02 +0300

    <client> add hooks to work with client data and client context

---
+++ client/src/Hooks/useClientProvider/useClientProvider.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+import useRouter from '../useRouter/useRouter';
+
+import {clientsMockData} from '../../mocks/data/clients';
+
+export default function useAuthProvider() {
+    const [client, setClient] = useState(null);
+  
+    // Set client to use in context
+    const setClientToContext = (clientInfo) => {
+        setClient(clientInfo);
+    };
+
+    const getClient = (clientId) => {
+        const searchClient = clientsMockData.filter((current) => current.id === Number.parseInt(clientId))[0];
+        if(searchClient) {
+            setClient(searchClient);
+            return true;
+        } else return false;    
+    };
+
+    const checkClientSet = (clientId) => {
+        if(client === null || client === false) {
+            return getClient(clientId);
+        } else {
+            if(Number.parseInt(clientId) !== client.id) {
+                return getClient(clientId);
+            }
+        }
+
+        return true;
+    }
+    
+    // Return the client object and clients methods
+    return {
+        client,
+        setClientToContext,
+        getClient,
+        checkClientSet
+    };
+}
