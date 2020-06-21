7cdd3f401f2f0afd6d742360648c9bd48c8ab24f
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-21 23:24:15 +0300

    <services> add simple project grpc service for clients api

---
+++ Microservices/Clients/Clients.API/Grpc/ClientsService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Clients.API.Grpc
+{
+    public class ClientsService : ClientsApi.ClientsApiBase
+    {
+        private readonly ILogger<ClientsService> _logger;
+        public ClientsService(ILogger<ClientsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ClientResponse> GetClientById(ClientRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Begin grpc call from method {Method} for basket id {Id}", context.Method, request.ClientId);
+
+            return Task.FromResult(new ClientResponse()
+            {
+                ClientId = 1,
+                ClientInn = 123,
+                ClientName = "Азаза",
+                ClientsHolding = "Ололо"
+            });
+        }
+    }
+}
--- Microservices/Clients/Clients.API/Startup.cs
+++ Microservices/Clients/Clients.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Clients.API.Grpc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,7 +32,7 @@ namespace Clients.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<ClientsService>();
 
                 endpoints.MapGet("/", async context =>
                 {
