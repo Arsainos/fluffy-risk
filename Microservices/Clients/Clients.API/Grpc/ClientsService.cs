0520f227ad83ef6a0638b13ca80d88aabe543cb1
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-22 17:38:39 +0300

    ovveride grpc service methods

--- Microservices/Clients/Clients.API/Grpc/ClientsService.cs
+++ Microservices/Clients/Clients.API/Grpc/ClientsService.cs
@@ -18,17 +18,56 @@ namespace Clients.API.Grpc
             _repository = repository;
         }
 
-        public override Task<ClientResponse> GetClientById(ClientRequest request, ServerCallContext context)
+        public override async Task<ClientActionResult> DeleteClient(ClientRequest request, ServerCallContext context)
         {
-            _logger.LogInformation("Begin grpc call from method {Method} for basket id {Id}", context.Method, request.ClientId);
+            _logger.LogInformation("Begin grpc call from method {Method} for client id {Id}", context.Method, request.ClientId);
 
-            return Task.FromResult(new ClientResponse()
+            var deleteResult = await _repository.DeleteClientInfoAsync(request.ClientId);
+
+            _logger.LogInformation("client {Id} have been deleted", request.ClientId);
+
+            return new ClientActionResult() { Result = deleteResult };
+        }
+
+        public override async Task<ClientResponse> GetClientById(ClientRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Begin grpc call from method {Method} for client id {Id}", context.Method, request.ClientId);
+
+            var clientData = await _repository.GetClientInfoAsync(request.ClientId);
+
+            _logger.LogInformation("Client data {info}", clientData);
+
+            return new ClientResponse()
             {
-                ClientId = 1,
-                ClientInn = 123,
-                ClientName = "Азаза",
-                ClientsHolding = "Ололо"
-            });
+                ClientId = clientData.Id,
+                ClientInn = clientData.Inn,
+                ClientName = clientData.Name,
+                ClientsHolding = clientData.Holding
+            };
+        }
+
+        public override Task<ListClientReponse> GetClients(ClintRequestWithNoParameters request, ServerCallContext context)
+        {
+            _logger.LogInformation("Begin grpc call from method {Method} for clients ", context.Method);
+
+            var clients = _repository.GetClients();
+
+            return null;
+        }
+
+        public override async Task<ClientResponse> UpdateClientInfo(ClientResponse request, ServerCallContext context)
+        {
+            _logger.LogInformation("Begin grpc call from method {Method} for client id {Id}", context.Method, request.ClientId);
+
+            var clientData = await _repository.UpdateClientInfoAsync(new ClientInfo() { Id = request.ClientId, Inn = request.ClientInn, Name = request.ClientName, Holding = request.ClientsHolding });
+
+            return new ClientResponse()
+            {
+                ClientId = clientData.Id,
+                ClientInn = clientData.Inn,
+                ClientName = clientData.Name,
+                ClientsHolding = clientData.Holding
+            };
         }
     }
 }
