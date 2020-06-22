1dfdbfc7dc8c0e6af8a173f707cc3c00080c5ed0
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-22 16:51:35 +0300

    <microservices> add models for Clients.Api

---
+++ Microservices/Clients/Clients.API/Model/ClientInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clients.API.Model
+{
+    public class ClientInfo
+    {
+        public int Id { get; set; }
+        public int Inn { get; set; }
+        public string Name { get; set; }
+        public string Holding { get; set; }
+    }
+}
---
+++ Microservices/Clients/Clients.API/Model/IClientsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clients.API.Model
+{
+    public interface IClientsRepository
+    {
+        Task<ClientInfo> GetClientInfoAsync(int clientId);
+        IEnumerable<ClientInfo> GetClients();
+        Task<ClientInfo> UpdateClientInfoAsync(ClientInfo clientInfo);
+        Task<bool> DeleteClientInfoAsync(int clientId);
+    }
+}
