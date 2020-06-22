25b8b887a137ff7d372935e0d5e3a3e31218ff87
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-22 18:01:12 +0300

    some configuration changes for service

--- Microservices/Clients/Clients.API/Startup.cs
+++ Microservices/Clients/Clients.API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Clients.API.Grpc;
+using Clients.API.Infrastructure.Repositories;
+using Clients.API.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +19,12 @@ namespace Clients.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
+            services.AddGrpc(options =>
+            {
+                options.EnableDetailedErrors = true;
+            });
+
+            services.AddTransient<IClientsRepository, DictionaryClientsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
