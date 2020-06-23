724eef3334bcfa5a312c39bfbab7d15ab12a3b6e
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-23 23:05:40 +0300

    add urls config class

---
+++ ApiGateway/Aggregator/WebApi.Aggregator/WebApi.Aggregator/Config/UrlsConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Aggregator.Config
+{
+    public class UrlsConfig
+    {
+        public string GrpcBasket { get; set; }
+    }
+}
