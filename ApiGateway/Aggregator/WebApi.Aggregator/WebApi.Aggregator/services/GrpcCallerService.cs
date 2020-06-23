96b77594602672598b801083a7bca4670df4dd06
Belyavtsev Artem <Arsainos@gmail.com>
2020-06-23 22:33:04 +0300

    add Grpc Calling Wrapper Service

---
+++ ApiGateway/Aggregator/WebApi.Aggregator/WebApi.Aggregator/services/GrpcCallerService.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Aggregator.services
+{
+    public static class GrpcCallerService
+    {
+        public static async Task<TResponse> CallService<TResponse>(string urlGrpc, Func<GrpcChannel, Task<TResponse>> func)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", true);
+
+            var channel = GrpcChannel.ForAddress(urlGrpc);
+
+            try
+            {
+                return await func(channel);
+            }
+            catch
+            {
+                return default;
+            }
+            finally
+            {
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", false);
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", false);
+            }
+        }
+
+        public static async Task CallService(string urlGrpc, Func<GrpcChannel, Task> func)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", true);
+
+            var channel = GrpcChannel.ForAddress(urlGrpc);
+
+            try
+            {
+                await func(channel);
+            }
+            finally
+            {
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", false);
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", false);
+            }
+        }
+    }
+}
