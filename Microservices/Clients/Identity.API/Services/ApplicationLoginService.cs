7b080260e282dc7f3c1b4f7c944cbb127baaf4fb
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-03 00:46:23 +0300

    add service layer with application login service

---
+++ Microservices/Clients/Identity.API/Services/ApplicationLoginService.cs
@@ -0,0 +1,42 @@
+﻿using Identity.API.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Services
+{
+    public class ApplicationLoginService : ILoginService<ApplicationUser>
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public ApplicationLoginService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public Task<ApplicationUser> FindByAccount(string account)
+        {
+            return null;
+        }
+
+        public Task SignIn(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SignInAsync(ApplicationUser user, AuthenticationProperties properties, string authenticationMethod = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> ValidateCredentials(ApplicationUser user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+    }
+}
