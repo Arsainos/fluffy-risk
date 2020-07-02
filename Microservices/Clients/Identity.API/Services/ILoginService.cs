2e741383cf3ef8643c12cf3042c3aa7a11d5994f
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-03 00:23:27 +0300

    Add login interface service

---
+++ Microservices/Clients/Identity.API/Services/ILoginService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Threading.Tasks;
+
+namespace Identity.API.Services
+{
+    public interface ILoginService<T>
+    {
+        Task<bool> ValidateCredentials(T user, string password);
+        Task<T> FindByAccount(string account);
+        Task SignIn(T user);
+        Task SignInAsync(T user, AuthenticationProperties properties, string authenticationMethod = null);
+    }
+}
