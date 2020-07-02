af0182f96fdd1d2f317ed96ccbfd2691f70dd3e4
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-02 23:05:19 +0300

    add application user model

---
+++ Microservices/Clients/Identity.API/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Models
+{
+    public class ApplicationUser
+    {
+        public string Account { get; set; }
+        public string LDAPAccount { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EMail { get; set; }
+    }
+}
