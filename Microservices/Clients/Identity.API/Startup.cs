99957506659e9117e4ae5d7a0c01ff3e2a9204f3
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-07 12:12:12 +0300

    add jwt token handler and symmetricsecurity key

--- Microservices/Clients/Identity.API/Startup.cs
+++ Microservices/Clients/Identity.API/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Identity.API
 {
@@ -18,6 +20,10 @@ namespace Identity.API
             Configuration = configuration;
         }
         public IConfiguration Configuration { get; }
+
+        private readonly JwtSecurityTokenHandler _jwtTokenHandler = new JwtSecurityTokenHandler();
+        private readonly SymmetricSecurityKey _securityKey = new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
