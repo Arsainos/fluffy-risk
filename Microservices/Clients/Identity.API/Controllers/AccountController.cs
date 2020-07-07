724ac3e538d1b6a391420e5bc8e8d63a6df332c2
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-07 19:02:20 +0300

    add login method to the controller

--- Microservices/Clients/Identity.API/Controllers/AccountController.cs
+++ Microservices/Clients/Identity.API/Controllers/AccountController.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Identity.API.Models;
+using Identity.API.Services;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Identity.API.Controllers
 {
@@ -11,5 +16,42 @@ namespace Identity.API.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly ILoginService<ApplicationUser> _loginService;
+        private readonly ILogger<AccountController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(ILoginService<ApplicationUser> loginService, ILogger<AccountController> logger, UserManager<ApplicationUser> userManager)
+        {
+            _loginService = loginService;
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _loginService.FindByLogin(model.Account);
+
+                if(await _loginService.ValidateCredentials(user, model.Password))
+                {
+                    var props = new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30),
+                        AllowRefresh = true,
+                        RedirectUri = model.ReturnUrl
+                    };
+
+                    await _loginService.SignInAsync(user, props);
+                }
+
+                return Redirect(model.ReturnUrl);
+            }
+
+            ModelState.AddModelError("", "Invalid username or password.");
+
+            return ValidationProblem();
+        }
     }
 }
---
+++ Microservices/Clients/Identity.API/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Models
+{
+    public class LoginModel
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
