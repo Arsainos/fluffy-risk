f2b9b643d388b711fee986198ed2e42f6b27e8c5
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-08 20:04:02 +0300

    add encoding

--- Microservices/Clients/Identity.API/Certificates/Keys.cs
+++ Microservices/Clients/Identity.API/Certificates/Keys.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace Identity.API.Certificates
 {
