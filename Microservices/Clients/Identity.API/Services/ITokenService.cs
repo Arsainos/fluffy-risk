959f3c377610aaba421d5bcb2501a73a57328637
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-08 12:39:02 +0300

    add token service

---
+++ Microservices/Clients/Identity.API/Services/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using Identity.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Identity.API.Services
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateJwtTokenAsync(string login, string password);
+        Task<List<Claim>> GetUserClaimsAsync(ApplicationUser user);
+    }
+}
---
+++ Microservices/Clients/Identity.API/Services/TokenService.cs
@@ -0,0 +1,60 @@
+﻿using Identity.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Identity.API.Services
+{
+    public class TokenService : ITokenService
+    {
+        readonly SignInManager<ApplicationUser> _signInManager;
+        readonly UserManager<ApplicationUser> _userManager;
+        readonly SymmetricSecurityKey _securityKey;
+        readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler;
+
+        public TokenService(SignInManager<ApplicationUser> signInManager, 
+            UserManager<ApplicationUser> userManager, 
+            SymmetricSecurityKey securityKey, 
+            JwtSecurityTokenHandler jwtSecurityTokenHandler)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _securityKey = securityKey;
+            _jwtSecurityTokenHandler = jwtSecurityTokenHandler;
+        }
+
+        public async Task<string> GenerateJwtTokenAsync(string login, string password)
+        {
+            ApplicationUser user = await _userManager.FindByNameAsync(login);
+
+            if (user == null || !user.PasswordHash.Equals(password))
+                throw new Exception("User not found");
+
+            await _signInManager.SignInAsync(user, false);
+
+            SigningCredentials credentials =
+                new SigningCredentials(_securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token =
+                new JwtSecurityToken("Fluffy-Risk.Identity.Api", "Fluffy-Risk.Clients", await GetUserClaimsAsync(user), expires: DateTime.Now.AddDays(1), signingCredentials: credentials);
+
+            return _jwtSecurityTokenHandler.WriteToken(token);
+        }
+
+        public async Task<List<Claim>> GetUserClaimsAsync(ApplicationUser user)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+
+            List<Claim> claims = new List<Claim>();
+
+            roles.ToList().ForEach(r => claims.Add(new Claim(ClaimTypes.Role, r)));
+
+            return claims;
+        }
+    }
+}
