ad338434a5b6ad3fc3957e5dd68b789d7e941075
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-09 13:10:25 +0300

    add accounts controller

---
+++ ApiGateway/Aggregator/WebApi.Aggregator/WebApi.Aggregator/Controllers/AccountsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Aggregator.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+    }
+}
