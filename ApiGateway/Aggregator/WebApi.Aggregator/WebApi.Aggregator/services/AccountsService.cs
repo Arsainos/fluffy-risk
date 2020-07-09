15d77a5949aec2e27d167a296df292c2f88a9c00
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-09 13:24:41 +0300

    add account grpc service

---
+++ ApiGateway/Aggregator/WebApi.Aggregator/WebApi.Aggregator/services/AccountsService.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApi.Aggregator.Models;
+using Identity.API;
+
+namespace WebApi.Aggregator.services
+{
+    public class AccountsService : IAccountsService
+    {
+        public readonly HttpClient _httpClient;
+        private readonly ILogger<AccountsService> _logger;
+
+        public AccountsService(HttpClient httpClient, ILogger<AccountsService> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public async Task<AccountInfo> GetAccountById(string id)
+        {
+            return await GrpcCallerService.CallServiceAsync("https://localhost:5002", async channel =>
+            {
+                var client = new AccountsGrpc.AccountsGrpcClient(channel);
+                _logger.LogInformation("grpc client created, request = { @id}", id);
+
+                var response = await client.GetAccountByIdAsync(new AccountRequest() { AccountId = id });
+                _logger.LogDebug("grpc response {@response}", response);
+
+                return await Task.FromResult(MapToAccountInfo(response));
+            });
+        }
+
+        public async Task<IEnumerable<AccountInfo>> GetAccounts()
+        {
+            return await GrpcCallerService.CallServiceAsync("https://localhost:5002", async channel =>
+            {
+                var client = new AccountsGrpc.AccountsGrpcClient(channel);
+                _logger.LogInformation("grpc get accounts");
+
+                var response = await client.GetAccountsAsync(new AccountRequestWithNoParameters());
+                _logger.LogDebug("grpc response {@response}", response);
+
+                return await Task.FromResult(response.Accounts.ToList().ConvertAll(new Converter<AccountResponse, AccountInfo>(MapToAccountInfo)).AsEnumerable());
+            });
+        }
+
+        public async Task<string> Login(string Name, string Password)
+        {
+            return await GrpcCallerService.CallServiceAsync("https://localhost:5002", async channel =>
+            {
+                var client = new AccountsGrpc.AccountsGrpcClient(channel);
+                _logger.LogInformation("grpc client created, request = { @id}", id);
+
+                var response = await client.LoginAsync(new AccountLoginRequest() { AccountName = Name, AccountPassword = Password });
+                _logger.LogDebug("grpc response {@response}", response);
+
+                return response.Token;
+            });
+        }
+
+        private static AccountResponse MapToAccountReponse(AccountInfo accountInfo)
+        {
+            AccountResponse account = new AccountResponse()
+            {
+                AccountId = accountInfo.Id,
+                AccountName = accountInfo.Name
+            };
+            account.AccountRole.Add(accountInfo.Roles);
+            return account;
+        }
+
+        private AccountInfo MapToAccountInfo(AccountResponse accountResponse)
+        {
+            return new AccountInfo
+            {
+                Id = accountResponse.AccountId,
+                Name = accountResponse.AccountName,
+                Roles = accountResponse.AccountRole.ToList()
+            };
+        }
+    }
+}
---
+++ ApiGateway/Aggregator/WebApi.Aggregator/WebApi.Aggregator/services/IAccountsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Aggregator.Models;
+
+namespace WebApi.Aggregator.services
+{
+    public interface IAccountsService
+    {
+        Task<AccountInfo> GetAccountById(string id);
+        Task<IEnumerable<AccountInfo>> GetAccounts();
+        Task<string> Login(string Name, string Password);
+    }
+}
