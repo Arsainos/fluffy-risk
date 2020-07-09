e6ab9ba8528abc1ca5fb6c70eada68983671d417
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-09 15:16:46 +0300

    add authenticate with jwt bearer and authenticate to swagger

--- ApiGateway/Aggregator/WebApi.Aggregator/WebApi.Aggregator/Startup.cs
+++ ApiGateway/Aggregator/WebApi.Aggregator/WebApi.Aggregator/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using WebApi.Aggregator.services;
 
@@ -20,9 +23,11 @@ namespace WebApi.Aggregator
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _securityKey = Certificates.Keys._securityKey;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly SymmetricSecurityKey _securityKey;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,6 +36,8 @@ namespace WebApi.Aggregator
 
             services.AddCustomMvc()
                 .AddApplicationServices();
+
+            services.AuthService(_securityKey);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +59,9 @@ namespace WebApi.Aggregator
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
@@ -64,18 +74,43 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddCustomMvc(this IServiceCollection services)
     {
-        services.AddSwaggerGen(options =>
-        { 
-            options.SwaggerDoc("v1", new OpenApiInfo
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
             {
                 Title = "Web Api Agregator for business app",
                 Version = "v1",
                 Description = "Web Api Agregator for business app"
             });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = $"JWT Authorization header using the Bearer scheme. Enter 'Bearer' {Environment.NewLine} [space] and then your token in the text input below. {Environment.NewLine} Example: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                In = ParameterLocation.Header,
+                Scheme = "Bearer"
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                    new List<string>()
+                }});
 
             var basePath = AppContext.BaseDirectory;
 
-            options.IncludeXmlComments(basePath + "\\WebApi.Aggregator.xml");
+            c.IncludeXmlComments(basePath + "\\WebApi.Aggregator.xml");
         });
 
         return services;
@@ -87,6 +122,36 @@ public static class ServiceCollectionExtensions
 
         services.AddHttpClient<IClientsService, ClientsService>();
         services.AddHttpClient<IAccountsService, AccountsService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AuthService(this IServiceCollection services, SymmetricSecurityKey securityKey)
+    {
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+            {
+                policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+                policy.RequireClaim(ClaimTypes.Role);
+            });
+        });
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters =
+                    new TokenValidationParameters
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        ValidateActor = false,
+                        ValidateLifetime = false,
+                        IssuerSigningKey = securityKey,
+                        RoleClaimType = ClaimTypes.Role.ToString()
+                    };
+            });
 
         return services;
     }
