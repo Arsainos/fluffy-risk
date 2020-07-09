98051788c7e78c076e43a4f068f2c54798201123
Belyavtsev Artem <Arsainos@gmail.com>
2020-07-09 12:02:46 +0300

    add grpc implementations

--- Microservices/Clients/Identity.API/Grpc/AccountsService.cs
+++ Microservices/Clients/Identity.API/Grpc/AccountsService.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Identity.API.Grpc
 {
-    public class AccountsService
+    public class AccountsService : AccountsGrpc.AccountsGrpcBase
     {
+        private readonly ILogger<AccountsService> _logger;
+
+        public AccountsService(ILogger<AccountsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AccountResponse> GetAccountById(AccountRequest request, ServerCallContext context)
+        {
+            return base.GetAccountById(request, context);
+        }
+
+        public override Task<ListAccountResponse> GetAccounts(AccountRequestWithNoParameters request, ServerCallContext context)
+        {
+            return base.GetAccounts(request, context);
+        }
+
+        public override Task<AccountTokenResponse> Login(AccountLoginRequest request, ServerCallContext context)
+        {
+            return base.Login(request, context);
+        }
     }
 }
